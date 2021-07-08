776b3330565046e3a1666e545ee4eb60e0aac1df
minchojoa <joonsungjung@gmail.com>
2021-07-09 00:46:01 +0900

    Solve

---
+++ Test/Q0001/A0001.java
@@ -0,0 +1,22 @@
+public class answer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count[] = new int[10];
+
+		for(int i = 1 ; i < 1001 ; i++) {
+			String strNum = Integer.toString(i);
+			for(int j = 0 ; j < strNum.length(); j++) {
+				int temp = Character.getNumericValue(strNum.charAt(j));
+				count[temp]++;
+//				System.out.println(strNum.charAt(j));
+			}
+		}
+
+		for(int i = 0 ; i < count.length ; i++) {
+			System.out.println(i+"의 갯수: " + count[i]);
+		}
+
+	}
+
+}
