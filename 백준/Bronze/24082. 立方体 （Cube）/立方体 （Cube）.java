fc235ad1e49acdcdddc98f9c629d0ae7d814c0c0
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-10-31 11:00:25 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 128 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int X = Integer.parseInt(br.readLine());
+		bw.write(Math.round(Math.pow(X,3))+"");
+		bw.close();
+	}
+}
