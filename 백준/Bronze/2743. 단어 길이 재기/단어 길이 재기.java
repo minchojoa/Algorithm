ba3e971b2444a169db17123cdc082e952e7dfffb
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-10-31 10:56:51 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String word = br.readLine();
+		int length = word.length();
+		bw.write(String.valueOf(length));
+		bw.close();
+	}	
+}
