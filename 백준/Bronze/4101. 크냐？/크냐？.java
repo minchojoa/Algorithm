1353e8977720e446fbb0817488e18ccbfcab89e8
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-10-31 10:56:57 +0900

    [Bronze V] Title: 크냐?, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while(true){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if(a>b){
+				bw.write("Yes\n");
+			}else if(a==0 && b==0){
+				break;
+			}else if(a<=b){
+				bw.write("No\n");
+			}
+		}
+		bw.close();
+	}
+}
