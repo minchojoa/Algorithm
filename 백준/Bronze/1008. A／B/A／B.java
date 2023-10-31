e51d101122b881a0d96055bfbe41350538136ec4
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-10-31 10:55:20 +0900

    [Bronze V] Title: A/B, Time: 216 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+            double ans = (double)A/B;
+			System.out.println(ans);
+			scan.close();
+	}//main
+}
