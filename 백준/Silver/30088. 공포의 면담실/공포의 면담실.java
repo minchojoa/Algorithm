a1b94e926f1d34a0cfa97f1d5838fe411f98e2bc
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-10-31 16:45:35 +0900

    [Silver V] Title: 공포의 면담실, Time: 780 ms, Memory: 205476 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -1,24 +1,29 @@
 import java.io.*;
 import java.util.*;
+import java.math.*;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int time[] = new int[N];
-		int result = 0;
+		BigInteger time[] = new BigInteger[N];
+		BigInteger result = new BigInteger("0");
 		for(int i = 0 ; i < N ; i++){
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int person = Integer.parseInt(st.nextToken());
 			for(int j = 0 ; j < person ; j++){
-				time[i] += Integer.parseInt(st.nextToken());
+				if(time[i] == null){
+					time[i] = new BigInteger("0");
+				}
+				time[i] = time[i].add(new BigInteger(st.nextToken()));
 			}
 		}
 		Arrays.sort(time);
 		for(int i = 0 ; i < N ; i++){
-			result += (N-i)*time[i];
+			BigInteger a = new BigInteger(String.valueOf(N-i));
+			BigInteger b = a.multiply(time[i]);
+			result = result.add(b);
 		}
 		System.out.print(result);
 	}
-
 }
