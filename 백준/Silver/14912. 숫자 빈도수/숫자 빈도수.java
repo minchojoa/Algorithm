eccf3aba871b8d63cac1dc8954e0d6ab06a59498
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-10-31 17:13:50 +0900

    [Silver V] Title: 숫자 빈도수, Time: 200 ms, Memory: 20436 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -9,9 +9,9 @@ public class Main {
 		int d = Integer.parseInt(st.nextToken());
 		int cnt = 0;
 		for(int i = 1 ; i < n+1 ; i++){
-			int length = String.valueOf(i).length();
-			for(int j = 0 ; j < length ; j++){
-				if(Character.getNumericValue(String.valueOf(i).charAt(j)) == d){
+			String num= String.valueOf(i);
+			for(int j = 0 ; j < num.length() ; j++){
+				if((num.charAt(j)-'0') == d){
 					cnt++;
 				}
 			}
