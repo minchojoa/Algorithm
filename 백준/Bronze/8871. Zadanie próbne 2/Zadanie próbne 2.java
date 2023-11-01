5082aedfb78bf57c558e81276e3c4c36859c3840
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-11-02 08:55:44 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 152 ms, Memory: 16028 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine())+1;
+		bw.write(n*2+" "+n*3);
+		bw.close();
+	}
+}
