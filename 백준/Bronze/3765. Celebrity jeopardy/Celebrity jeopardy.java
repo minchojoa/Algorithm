d30a0d5738a914cbaf363574a384e20a007f0902
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-11-05 13:52:28 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 128 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String s;
+        while((s=br.readLine())!=null){
+            bw.write(s+"\n");
+        }
+        bw.close();
+    }
+}
