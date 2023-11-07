df9d5f863a9151f6774ab3ea41d011735c02c4df
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-11-08 08:26:57 +0900

    [Bronze IV] Title: Dedupe, Time: 116 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/5357. Dedupe/Dedupe.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < N ; i++){
+			String s = br.readLine();
+			for(int j = 0 ; j < s.length() ; j++){
+				if(j==0){
+					bw.write(s.charAt(j));
+				}else if(!(s.charAt(j)==s.charAt(j-1))){
+					bw.write(s.charAt(j));
+				}
+			}
+			bw.newLine();
+		}
+		bw.close();
+	}
+}
