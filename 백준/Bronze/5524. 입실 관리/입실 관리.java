b1d0ae77060be611cd57ee60c4a606709645d5c0
minchojoa <54099997+minchojoa@users.noreply.github.com>
2023-11-16 14:00:11 +0900

    [Bronze IV] Title: 입실 관리, Time: 132 ms, Memory: 14312 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < N ; i++){
+			String name = br.readLine().toLowerCase();
+			bw.write(name+"\n");
+		}
+		bw.close();
+	}
+}
