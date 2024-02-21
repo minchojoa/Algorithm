7a443286480fe0032ee14b896c17e36fbfa4b5d2
minchojoa <54099997+minchojoa@users.noreply.github.com>
2024-02-21 15:40:01 +0900

    [Silver V] Title: 역원소 정렬, Time: 564 ms, Memory: 36460 KB -BaekjoonHub

---
+++ 백준/Silver/5648. 역원소 정렬/역원소 정렬.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Long num[] = new Long[n];
+
+		for(int i = 0 ; i < n ; i++){
+			long input = sc.nextLong();
+			String temp = "";
+			int len = String.valueOf(input).length();
+			for(int j = 0 ; j < len ; j++){
+				temp += String.valueOf(input%10);
+				input /= 10;
+			}
+			num[i] = Long.parseLong(temp);
+		}
+		sc.close();
+		Arrays.sort(num);
+		for(Long i : num){
+			bw.write(i+"\n");
+		}
+		bw.close();
+	}
+}
