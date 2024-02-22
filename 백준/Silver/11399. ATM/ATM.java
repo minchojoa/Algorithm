885eeb36d8bbddf760674701963b23537d692297
minchojoa <54099997+minchojoa@users.noreply.github.com>
2024-02-22 13:19:53 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,32 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static int time[]; 
+	static int memo[]; 
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int time[] = new int[N];
+		time = new int[N];
+		memo = new int[N];
 		int result = 0;
 		for(int i = 0 ; i < N ; i++){
 			time[i] = Integer.parseInt(st.nextToken());
 		}
-
 		Arrays.sort(time);
-
 		for(int i = 0 ; i < N ; i++){
-			result += getTime(time, i, time[0]);
+			result += getTime(i);
 		}
 		bw.write(result+"");
 		bw.close();
 	}
-
-	public static int getTime(int[]time, int index, int sum){
+	
+	public static int getTime(int index){
+		if(memo[index]>0){
+			return memo[index];
+		}
+		
 		if(index==0){
-			return sum;
+			memo[index] = time[0];
+			return memo[index];
+		}else if(index==1){
+			memo[index] = memo[index-1] + time[index];
+			return memo[index];
 		}else{
-			sum += time[index];
-			return getTime(time, index-1, sum);
+			memo[index] = getTime(index-1) + time[index];
+			return memo[index];
 		}
 	}
 }
