ed963db560f23e4a9ea09771f7c1246c2fbe5f30
minchojoa <54099997+minchojoa@users.noreply.github.com>
2024-02-27 17:27:42 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 276 ms, Memory: 57208 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Main {
-	static int primeList[] = new int[10000002]; // 0: 초기화 1: 소수o 2: 소수x
+	static int primeList[] = new int[2000000]; // 0: 초기화 1: 소수o 2: 소수x
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
