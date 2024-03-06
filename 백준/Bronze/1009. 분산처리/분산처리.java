5c2e6372ee12fcf8e04eafa141fa01ef7dcd8e05
minchojoa <54099997+minchojoa@users.noreply.github.com>
2024-03-06 11:26:02 +0900

    [Bronze II] Title: 분산처리, Time: 1024 ms, Memory: 17192 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		for(int i = 0 ; i < T ; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int data = 1;
+
+			for(int j = 0 ; j < b ; j++){
+				data = (data*a)%10;
+			}
+			
+			if(data==0){
+				bw.write("10\n");
+			}else{
+				bw.write(data+"\n");
+			}
+		}
+		bw.close();
+	}
+}
